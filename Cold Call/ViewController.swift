4f33654c64d42b81d513a6a3c908de09a80701f0
Nishant Patel <contact@nishant-patel.com>
2017-03-08 16:48:31 -0800

    Cold Commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let names = ["Nishant", "Arpit", "Parth", "Anu", "Marcus", "Eli"]
+    
+
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var numberDisplay: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        topLabel.text = "Ready?"
+        numberDisplay.isHidden = true
+        
+    }
+        
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        
+        let namesIndex = Int(arc4random_uniform(5) + 1)
+        print(namesIndex)
+        topLabel.text = names[namesIndex]
+        numberDisplay.text = String(namesIndex)
+        numberDisplay.isHidden = false
+        if namesIndex == 1 || namesIndex == 2 {
+            numberDisplay.textColor = UIColor.red
+        } else if namesIndex == 3 || namesIndex == 4 {
+            numberDisplay.textColor = UIColor.orange
+        }  else if namesIndex == 5 {
+            numberDisplay.textColor = UIColor.green
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
